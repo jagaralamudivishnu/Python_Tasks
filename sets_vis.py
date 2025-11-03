59849fc4abba3798442d2647a468b63671679006
jagaralamudivishnu <119559721+jagaralamudivishnu@users.noreply.github.com>
2025-11-03 07:26:32 +0530

    local storage
    Python tasks

---
+++ sets_vis.py
@@ -0,0 +1,69 @@
+#-----------Sets-----------------
+
+#Task 1: Set intersection
+#write a python code to find and print the intersection of the following two sets:
+
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# #output should be: {4,5}
+# set3=set1.intersection(set2)
+# print(set3)
+
+#Task 2:Set Union 
+#write Python code to find and print the union of the following two sets:
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# #output should be: {1,2,3,4,5,6,7,8}
+# set3=set1.union(set2)
+# print(set3)
+
+#Task 3: Set Difference
+#write Python code to find and print the elements present in SET1 but not in SET2:
+# set1={1,2,3,4,5}
+# set2 = {4,5,6,7,8}
+# #output should be:{1,2,3}
+# set3=set1.difference(set2)
+# print(set3)
+
+#Task 4: SEt Symmetric Difference
+#write python code to find and print the symmertic difference of the following two sets:
+
+# set1={1,2,3,4,5}
+# set2={4,5,6,7,8}
+# #output should be: {1,2,3,6,7,8}
+# set3=set1.symmetric_difference(set2)
+# print(set3)
+
+#Task5: Set Membership Test.
+
+#write python code to check if the element 3 is present in the set my_set:\
+
+# my_set ={1,2,3,4,5}
+# print( 3 in my_set )
+
+#excercise 1: Set intersection.
+# my_set1 ={10,20,39,40,50}
+# my_set2 ={30,34,454,23}
+# my_set3= my_set1.intersection(my_set2)
+# print(my_set3)
+
+#excercise 2: Set Union
+# my_set1 ={10,29,34,998}
+# my_set2 ={45,34,234,45}
+# my_set3 = my_set1.union(my_set2)
+# print(my_set3)
+
+#excercise 3: Set Difference
+# my_set1 ={100,23,898}
+# my_set2 ={232,789,23,90}
+# my_set3 = my_set1.difference(my_set2)
+# print(my_set3)
+
+#excerise 4: Set Symmetric Difference
+#write a python script that finds and prints the symmetric difference between two sets.
+
+# my_set1={12,23,564,44}
+# my_set2={232,44,23,90}
+# my_set3 = my_set1.symmetric_difference(my_set2)
+# print(my_set3)
+
